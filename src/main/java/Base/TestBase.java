f4a86971a6e01ce08785975927fb61b3262861e0
unknown <shubham.verma@aa.onmicrosoft.com>
2021-03-12 14:28:46 +0530

    removed unsed packages

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -4,17 +4,13 @@ import Utilities.TestUtil;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.sun.xml.internal.ws.resources.UtilMessages;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
-import javax.xml.bind.SchemaOutputResolver;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
